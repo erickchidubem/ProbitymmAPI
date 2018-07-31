86fbec47e4011093a74e446ecd9dfdf20850ee48
erickchidubem <erickchidubem@gmail.com>
2018-07-31 09:42:10 +0100

    New Commits

--- ProbitymmAPI/Models/AuthenticateModel.cs
+++ ProbitymmAPI/Models/AuthenticateModel.cs
@@ -58,4 +58,11 @@ namespace ProbitymmAPI.Models
         public int DepartmentName { get; set; }
     }
 
+    public class VerifyData
+    {
+        public int BusinessId { get; set; }
+        public int UserId { get; set; }
+        public string LastPasswordChange { get; set; }
+    }
+
 }
